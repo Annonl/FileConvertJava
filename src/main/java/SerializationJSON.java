018ac355142ff736c17d459fcb045edf1ef195b1
Alexey <alesha.kolpashikov@gmail.com>
2021-11-06 15:38:02 +0300

    write serialize to json

--- src/main/java/SerializationJSON.java
+++ src/main/java/SerializationJSON.java
@@ -1,2 +1,17 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class SerializationJSON {
+    public static void SerializeObject(Shop shop, String fileName) throws IOException {
+        StringWriter writer = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(writer, shop);
+        String result = writer.toString();
+        FileWriter writer1 = new FileWriter(fileName, false);
+        writer1.write(result);
+        writer1.flush();
+    }
 }
